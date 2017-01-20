7e6bd8569cf104703bbbf08ba60945176a97e605
Hrvoje Hodak <dev@tribly.de>
2017-01-20 11:16:16 +0100

    Use new ruby version

--- exports.zsh
+++ exports.zsh
@@ -7,4 +7,4 @@ export LS_COLORS="rs=0:di=01;34:ln=01;36:mh=00:pi=40;33:so=01;35:do=01;35:bd=40;
 
 
 export PATH=$PATH:/home/$(whoami)/scripts
-export PATH=$PATH:/home/$(whoami)/.gem/ruby/2.3.0/bin
+export PATH=$PATH:/home/$(whoami)/.gem/ruby/2.4.0/bin
